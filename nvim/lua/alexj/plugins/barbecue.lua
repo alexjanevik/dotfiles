881244fcbb325243d3b0eb708a937b1d569d35a9
Alex Janevik <ajanevik@gmail.com>
2024-07-26 11:15:27 +1000

    adds barbecue.lua for an ide-like winbar

---
+++ nvim/lua/alexj/plugins/barbecue.lua
@@ -0,0 +1,13 @@
+return {
+	"utilyre/barbecue.nvim",
+	name = "barbecue",
+	after = "catppuccin",
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	opts = {
+		-- configurations go here
+	},
+}
