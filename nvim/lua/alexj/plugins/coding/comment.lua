9941646b087e0269bf5438c77786f3df721c100e
Alex Janevik <ajanevik@gmail.com>
2026-08-03 13:06:58 +1000

    adds fallback option for comment.nvim for C++ files

--- nvim/lua/alexj/plugins/coding/comment.lua
+++ nvim/lua/alexj/plugins/coding/comment.lua
@@ -13,7 +13,19 @@ return {
 		-- enable comment
 		comment.setup({
 			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			--pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = function(ctx)
+				-- Run the default context hook first
+				local hook = ts_context_commentstring.create_pre_hook()
+				local result = hook(ctx)
+
+				-- Fallback manually if the result is nil for C/C++ files
+				if not result and (vim.bo.filetype == "cpp" or vim.bo.filetype == "c") then
+					return "// %s"
+				end
+
+				return result
+			end,
 		})
 	end,
 }
